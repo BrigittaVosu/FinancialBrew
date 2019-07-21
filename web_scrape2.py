3566f01c26f82eeca00fde9310b3478f7fbb4ee0
Brigitta Vosu <brigitta.vosu@gmail.com>
2019-07-21 19:07:23 -0400

    additional columns

--- web_scrape2.py
+++ web_scrape2.py
@@ -8,7 +8,7 @@ req = urllib.request.Request(rank_page, headers={'User-Agent': 'Mozilla/5.0 (Mac
 page = urllib.request.urlopen(req)
 soup = BeautifulSoup(page, 'html.parser')
 
-divTags = soup.find('div', attrs={'class': 'Mb(25px)'}).find_all('div')
+divTags = soup.find('div', attrs={'class': 'Mb(25px)', 'data-reactid': '56'}).find_all('div')
 
 # malox_raw = row.find('div', attrs={'style': 'float: left; width: 80px;'})
 # if malox_raw is not None:
@@ -21,7 +21,7 @@ divTags = soup.find('div', attrs={'class': 'Mb(25px)'}).find_all('div')
 # writer.writerow(['Return', 'MALOX', 'Category'])
 
 for divTag in divTags:
-    perfTitle = divTag.find('span', attrs={'class': 'Fl(start)'})
+    perfTitle = divTag.find('span', attrs={'class': 'W(50%) D(b) Fl(start) Ta(s)'})
     # if perfTitle is not None:
     #     perfTitle = perfTitle.span.text.strip()
     if perfTitle is not None:
@@ -32,13 +32,8 @@ for divTag in divTags:
                 except:
                     pass
     
-    # views = channel.find('span', attrs={'class': 'Fl(end)'})
-    # if views is not None:
-    #     for view in views:
-    #         if view is not None:
-    #             print(view)
 
-    performances = divTag.find('span', attrs={'class': 'Fl(end)'})
+    performances = divTag.find('span', attrs={'class': 'W(20%) D(b) Fl(start) Ta(e)'})
     if performances is not None:
         for row2 in performances:
             if row2 is not None:
@@ -46,12 +41,6 @@ for divTag in divTags:
                     row2 = row2.text.strip()
                 except:
                     pass
-                # print(view)
-                # print(type(view))
-                # print(view.text.strip())
-                # strippedView = view.span.text.strip()
-                # print(strippedView)
-                # print(type(strippedView))
         
     #views = views.span.text.strip()
     #views = channel.find_all('span', attrs={'class': 'Fl(start)'}).span.text.strip()
